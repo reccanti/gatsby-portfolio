ec9b1a771d23a5d12bbbff6130ab5446355a8bbf
Ben Wilcox <reccanti@gmail.com>
2018-03-05 08:08:39 -0500

    Replace PaletteControl with List
    The PaletteControl component, which was just a wrapper for a List, was
    throwing an error, so I replaced with a standard ListControl

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,4 @@ import React from 'react'
 import CMS from 'netlify-cms'
 import { PaletteControl, PalettePreview } from '../components/palette-blog/palette-cms'
 
-CMS.registerWidget("palette", PaletteControl, PalettePreview)
+CMS.registerWidget("palette", CMS.getWidget("list").control, PalettePreview)
---
+++ src/components/palette-blog/palette-blog-cms.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import CMS from 'netlify-cms'
+
+import { Palette, Color } from './palette' 
+
+let ListControl = CMS.getWidget("list").control
+
+const PaletteControl = props => {
+    return <ListControl {...props} />
+}
+
+const PalettePreview = props => {
+    return (
+        <Palette width="100%" height={100} title={`Color Palette`}>
+            {props.value.map(color => (
+                <Color hex={color.get("color")} size={Number(color.get("width"))} />
+            ))}
+        </Palette>
+    )
+}
+
+export { PaletteControl, PalettePreview }
